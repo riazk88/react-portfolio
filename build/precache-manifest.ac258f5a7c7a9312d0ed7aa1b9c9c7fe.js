fc649ac46d1eb86c8c085fd846f83ea44da32e15
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-08-14 21:39:03 -0600

    gh pages

---
+++ build/precache-manifest.ac258f5a7c7a9312d0ed7aa1b9c9c7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aacad938395cc2164c19e4f722d397c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "34fd3b4082f309cfb443",
+    "url": "/react-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "508971a40b002be3fec4",
+    "url": "/react-portfolio/static/css/main.c6d53da7.chunk.css"
+  },
+  {
+    "revision": "34fd3b4082f309cfb443",
+    "url": "/react-portfolio/static/js/2.35ac5fab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.35ac5fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508971a40b002be3fec4",
+    "url": "/react-portfolio/static/js/main.43c10cfe.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
